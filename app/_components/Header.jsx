d6f53b6e844d57f1ae1370abe265e6077108f94e
hky404 <hky404@hky404s-MacBook-Pro.local>
2024-05-19 13:02:09 -0700

    fixing the navbar

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -29,7 +29,7 @@ function Header() {
 
   return (
     <div className='p-6 px-10 flex justify-between shadow-sm fixed top-0 w-full z-10 bg-white'>
-      <div className='flex gap-12 items-center'>
+      <div className='flex gap-8 items-center'>
         <Link href={'/'}>
           <Image src={'/logo.svg'} width={100} height={100} alt='logo' />
         </Link>
